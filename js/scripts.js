04d6192ae6e99dfb4d0d8a168e7d4583aeeb641c
simplic2 <anagael128@gmail.com>
2026-07-14 21:43:05 -0300

    Delete js directory

--- js/scripts.js
+++
@@ -1,7 +0,0 @@
-// Configurações globais e inicialização auxiliar
-const ZapConfig = {
-    version: "1.0.0",
-    theme: "dark"
-};
-
-console.log(`ZapPanel carregado com sucesso - v${ZapConfig.version}`);
